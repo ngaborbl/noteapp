2a70bea561b8def2af748ebab90a7708b92dec93
Nagy Gábor <ngabor.blelle@gmail.com>
2024-11-07 18:44:47 +0100

    feat: add FCM support and fix file structure V3

--- service-worker.js
+++ service-worker.js
@@ -1,15 +1,11 @@
 importScripts('https://www.gstatic.com/firebasejs/9.6.0/firebase-app-compat.js');
 importScripts('https://www.gstatic.com/firebasejs/9.6.0/firebase-messaging-compat.js');
 
-// Firebase inicializálása
 firebase.initializeApp({
   apiKey: "AIzaSyBsQMs29I_kwN5idgcyAdz0etWfv7ymyz8",
-  authDomain: "noteapp-5c98e.firebaseapp.com",
   projectId: "noteapp-5c98e",
-  storageBucket: "noteapp-5c98e.appspot.com",
   messagingSenderId: "10607490745",
-  appId: "1:10607490745:web:5cdff4c9c5e78d7c798d68",
-  measurementId: "G-3NSSJ1FT7S"
+  appId: "1:10607490745:web:5cdff4c9c5e78d7c798d68"
 });
 
 const messaging = firebase.messaging();
